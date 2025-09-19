d3ee2a439e865ef79acaee4507db67cd2ac772bd
Umar farook <154865642+StrawHatCoderz@users.noreply.github.com>
2025-09-19 17:39:49 +0530

    update logic for all double digit values

--- reverse.js
+++ reverse.js
@@ -1,9 +1,14 @@
-const numberToReverse = 9;
+const numberToReverse = 10;
 let numberToProcess = numberToReverse;
 let reversedNumber = 0;
 
-const lastDigit = numberToReverse % 10;
+let lastDigit = numberToReverse % 10;
 reversedNumber = reversedNumber * 10 + lastDigit;
 numberToProcess = numberToProcess / 10;
+
+lastDigit = numberToReverse % 10;
+reversedNumber = reversedNumber * 10 + lastDigit;
+numberToProcess = numberToProcess / 10;
+console.log(numberToProcess);
 
 console.log('reverse of',numberToReverse,'is',reversedNumber);
