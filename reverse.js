6d5b3b30faeccc2c759dccba208f7d76dc03e2f4
Umar farook <154865642+StrawHatCoderz@users.noreply.github.com>
2025-09-19 18:02:32 +0530

    update reverse logic for more than one digit number

--- reverse.js
+++ reverse.js
@@ -1,13 +1,11 @@
-const numberToReverse = 10;
+const numberToReverse = 3210;
 let numberToProcess = numberToReverse;
 let reversedNumber = 0;
 
-let lastDigit = numberToProcess % 10;
-reversedNumber = reversedNumber * 10 + lastDigit;
-numberToProcess = numberToProcess / 10 - (lastDigit / 10);
-
-lastDigit = numberToProcess % 10;
-reversedNumber = reversedNumber * 10 + lastDigit;
-numberToProcess = numberToProcess / 10 - (lastDigit / 10);
+while(numberToProcess !== 0) {
+  const lastDigit = numberToProcess % 10;
+  reversedNumber = reversedNumber * 10 + lastDigit;
+  numberToProcess = numberToProcess / 10 - (lastDigit / 10);
+}
 
 console.log('reverse of',numberToReverse,'is',reversedNumber);
