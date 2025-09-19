62393caa2b210349b52affc2f24248a635066332
Umar farook <154865642+StrawHatCoderz@users.noreply.github.com>
2025-09-19 17:02:27 +0530

    add basic palindrome check using variables

--- palindrome.js
+++ palindrome.js
@@ -1,2 +1,5 @@
 const numberToCheckPalindrome = 1;
+const reversedNumber = numberToCheckPalindrome;
+const isPalindrome = reversedNumber === numberToCheckPalindrome;
+const suffix = isPalindrome ? 'is palindrome' : "not a palindrome";
 console.log(numberToCheckPalindrome, "is palindrome");
