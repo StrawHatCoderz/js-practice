68e7f8464559efd780b721303beb689cbf72a58d
Umar farook <154865642+StrawHatCoderz@users.noreply.github.com>
2025-09-19 18:08:43 +0530

    replace hardcode suffix with variable

--- palindrome.js
+++ palindrome.js
@@ -2,4 +2,4 @@ const numberToCheckPalindrome = 1;
 const reversedNumber = numberToCheckPalindrome;
 const isPalindrome = reversedNumber === numberToCheckPalindrome;
 const suffix = isPalindrome ? 'is palindrome' : "not a palindrome";
-console.log(numberToCheckPalindrome, "is palindrome");
+console.log(numberToCheckPalindrome,suffix);
