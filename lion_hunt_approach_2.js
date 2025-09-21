7235ce971b0914b7988cd04a13febea031d2c130
Umar farook <154865642+StrawHatCoderz@users.noreply.github.com>
2025-09-21 11:33:02 +0530

    write basic logic to find lion and zebra

--- lion_hunt_approach_2.js
+++ lion_hunt_approach_2.js
@@ -1,9 +1,39 @@
-const testCase1 = "LZ";
-const testCase2 = "L Z";
-const testCase3 = "L ZL";
+const testCase1 = "L";
+const testCase2 = "Z";
+const testCase3 = " ";
+const testCase4 = "L  L";
+const testCase5 = "Z  Z";
 
-const testCaseToUse = testCase1;
-const closestDistance = 0;
+const testCaseToUse = testCase4;
+let closestDistance = -1;
 
+const lion = "L";
+const zebra = "Z";
+
+let lionFound = false;
+let zebraFound = true;
+
+if(testCaseToUse.length > 1) {
+  let positionSerching = testCaseToUse[0]
+  if(positionSerching === lion) {
+    lionFound = true;
+  } else if(positionSerching === zebra) {
+    zebraFound = false;
+  }
+
+  positionSerching = testCaseToUse[1]
+  if(positionSerching === lion) {
+    lionFound = true;
+  } else if(positionSerching === zebra) {
+    zebraFound = false;
+  }
+
+  positionSerching = testCaseToUse[2]
+  if(positionSerching === lion) {
+    lionFound = true;
+  } else if(positionSerching === zebra) {
+    zebraFound = false;
+  }
+}
 
 console.log("Input: ", testCaseToUse, "Output: ",closestDistance);
