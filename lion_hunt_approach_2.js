a27f2fc5f9c3a3e9faa2025d7ccf0901f577cf43
Umar farook <154865642+StrawHatCoderz@users.noreply.github.com>
2025-09-21 11:23:01 +0530

    setup basic format

---
+++ lion_hunt_approach_2.js
@@ -0,0 +1,9 @@
+const testCase1 = "LZ";
+const testCase2 = "L Z";
+const testCase3 = "L ZL";
+
+const testCaseToUse = testCase1;
+const closestDistance = 0;
+
+
+console.log("Input: ", testCaseToUse, "Output: ",closestDistance);
