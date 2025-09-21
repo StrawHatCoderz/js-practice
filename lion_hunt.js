ec06f415c822a78dfbffc10549b5e82d3f0b46d2
Umar farook <154865642+StrawHatCoderz@users.noreply.github.com>
2025-09-21 10:52:18 +0530

    move calculating distance logic to loop

--- lion_hunt.js
+++ lion_hunt.js
@@ -1,5 +1,5 @@
 const testCase1 = "L";
-const testCase2 = "L Z";
+const testCase2 = "L ZL Z";
 const testCase3 = "L ZL";
 const testCaseToUse = testCase2;
 
@@ -9,21 +9,24 @@ const zebra = "Z";
 let lionPosition = -1;
 let zebraPosition = -1;
 
+let closestDistance = -1;
+
 for (let index = 0; index < testCaseToUse.length; index++) {
    if(testCaseToUse[index] === lino) {
       lionPosition = index;
    } else if(testCaseToUse[index] === zebra) {
       zebraPosition = index
    }
-}
 
-let closestDistance = -1;
-if(lionPosition !== -1 && zebraPosition !== -1) { 
+   if(lionPosition !== -1 && zebraPosition !== -1) {
    if(lionPosition > zebraPosition) {
       closestDistance = lionPosition - zebraPosition - 1;
    } else {
       closestDistance = zebraPosition - lionPosition - 1;
    }
 }
+}
+
+
 
 console.log("Input:",testCaseToUse, "Output:",closestDistance);
