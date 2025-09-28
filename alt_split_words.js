f8279f49cdd918b1f9f025ce2d997e0137b49d53
Umar farook <154865642+StrawHatCoderz@users.noreply.github.com>
2025-09-28 10:41:59 +0530

    add test function for testing

--- alt_split_words.js
+++ alt_split_words.js
@@ -1,9 +1,32 @@
-function testAltSplitWords(text, expectedOutput) {
+function symbol(result, expectedOutput) {
+  return result === expectedOutput ? '✅' : '❌';
+}
 
+function displayResults(text, result, expectedOutput) {
+  const mark = symbol(result, expectedOutput);
+  const inputSection = '[ ' + text + ' ]';
+  const expectedSection = 'Expected: [ ' + expectedOutput + ' ]';
+  const actualSection = 'Actual: [ ' + result + ' ]';
+
+  let message = mark;
+  message += ' | ' + inputSection;
+  message += ' | ' + expectedSection;
+  message += ' | ' + actualSection;
+
+  console.log(message);
+}
+
+function altSplitWords() {
+  return 'ape,p,l';
+}
+
+function testAltSplitWords(text, expectedOutput) {
+  const result = altSplitWords(text);
+  displayResults(text, result, expectedOutput)
 }
 
 function main() {
-
+  testAltSplitWords('apple', 'ape,p,l');
 }
 
 main();
